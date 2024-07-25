77894cb42cf606af7298a61d328287fe367ce6f1
pxai <pello.altadill@gmail.com>
2024-07-25 14:40:06 +0200

    Added

--- src/07/testing/kalkulagailua.test.py
+++ src/07/testing/kalkulagailua.test.py
@@ -2,21 +2,21 @@ import unittest
 import kalkulagailua
 
 class TestStringMethods(unittest.TestCase):
-    def test_batura(self):
+    def test_batu(self):
         kal = kalkulagailua.Kalkulagailua()
-        self.assertEqual(kal.batura(40, 2), 42)
+        self.assertEqual(kal.batu(40, 2), 42)
 
-    def test_restar(self):
+    def test_kendu(self):
         kal = kalkulagailua.Kalkulagailua()
         self.assertEqual(kal.kendu(40, 2), 38)
 
-    def test_multiplicar(self):
+    def test_biderkatu(self):
         kal = kalkulagailua.Kalkulagailua()
-        self.assertEqual(c.biderkatu(40, 2), 80)
+        self.assertEqual(kal.biderkatu(40, 2), 80)
 
-    def test_dividir(self):
+    def test_zatitu(self):
         kal = kalkulagailua.Kalkulagailua()
-        self.assertEqual(c.zatitu(40, 2), 20)
+        self.assertEqual(kal.zatitu(40, 2), 20)
 
 if __name__ == '__main__':
     unittest.main()

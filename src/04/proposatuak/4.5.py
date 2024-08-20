e705659c99c6f9011b88123759db2dcb619423f7
pxai <pello.altadill@gmail.com>
2024-08-20 10:07:29 +0200

    Almost there

--- src/04/proposatuak/4.5.py
+++ src/04/proposatuak/4.5.py
@@ -4,14 +4,14 @@ def ausazkoa(max):
     return random.randint(0, max - 1)
 
 def izenaSortu(silabak):
-    oinak = ["a", "e", "i", "o", "u"]
+    bokalak = ["a", "e", "i", "o", "u"]
     konsonanteak = ["b","c","d","f","g","h","j","k","l","m","n","ñ","p","q","r","s","t","v","w","x","y","z"]
     izena = ""
 
     for i in range(silabak):
         konsonantea = konsonanteak[ausazkoa(len(konsonanteak))]
-        oina = oinak[ausazkoa(len(oinak))]
-        izena = izena + konsonantea + oina
+        bokala = bokalak[ausazkoa(len(bokalak))]
+        izena = izena + konsonantea + bokala
 
     return izena
 
